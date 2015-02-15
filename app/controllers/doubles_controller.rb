95aa9c5fe14e7f0b08045b8225e4f989bf3c4f41
polarpine <vanessabiggers@gmail.com>
2015-02-15 13:45:57 -0800

    add more hymns

---
+++ app/controllers/doubles_controller.rb
@@ -0,0 +1,208 @@
+class DoublesController < ApplicationController
+  def d1
+  end
+
+  def d2
+  end
+
+  def d3
+  end
+
+  def d4
+  end
+
+  def d5
+  end
+
+  def d6
+  end
+
+  def d7
+  end
+
+  def d8
+  end
+
+  def d9
+  end
+
+  def d10
+  end
+
+  def d11
+  end
+
+  def d12
+  end
+
+  def d13
+  end
+
+  def d14
+  end
+
+  def d15
+  end
+
+  def d16
+  end
+
+  def d17
+  end
+
+  def d18
+  end
+
+  def d19
+  end
+
+  def d20
+  end
+
+  def d21
+  end
+
+  def d22
+  end
+
+  def d23
+  end
+
+  def d24
+  end
+
+  def d25
+  end
+
+  def d26
+  end
+
+  def d27
+  end
+
+  def d28
+  end
+
+  def d29
+  end
+
+  def d30
+  end
+
+  def d31
+  end
+
+  def d32
+  end
+
+  def d33
+  end
+
+  def d34
+  end
+
+  def d35
+  end
+
+  def d36
+  end
+
+  def d37
+  end
+
+  def d38
+  end
+
+  def d39
+  end
+
+  def d40
+  end
+
+  def d41
+  end
+
+  def d42
+  end
+
+  def d43
+  end
+
+  def d44
+  end
+
+  def d45
+  end
+
+  def d46
+  end
+
+  def d47
+  end
+
+  def d48
+  end
+
+  def d49
+  end
+
+  def d50
+  end
+
+  def d51
+  end
+
+  def d52
+  end
+
+  def d53
+  end
+
+  def d54
+  end
+
+  def d55
+  end
+
+  def d56
+  end
+
+  def d57
+  end
+
+  def d58
+  end
+
+  def d59
+  end
+
+  def d60
+  end
+
+  def d61
+  end
+
+  def d62
+  end
+
+  def d63
+  end
+
+  def d64
+  end
+
+  def d65
+  end
+
+  def d66
+  end
+
+  def d67
+  end
+
+  def d68
+  end
+
+  def d69
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,142 @@
 Rails.application.routes.draw do
+  get 'doubles/d1'
+
+  get 'doubles/d2'
+
+  get 'doubles/d3'
+
+  get 'doubles/d4'
+
+  get 'doubles/d5'
+
+  get 'doubles/d6'
+
+  get 'doubles/d7'
+
+  get 'doubles/d8'
+
+  get 'doubles/d9'
+
+  get 'doubles/d10'
+
+  get 'doubles/d11'
+
+  get 'doubles/d12'
+
+  get 'doubles/d13'
+
+  get 'doubles/d14'
+
+  get 'doubles/d15'
+
+  get 'doubles/d16'
+
+  get 'doubles/d17'
+
+  get 'doubles/d18'
+
+  get 'doubles/d19'
+
+  get 'doubles/d20'
+
+  get 'doubles/d21'
+
+  get 'doubles/d22'
+
+  get 'doubles/d23'
+
+  get 'doubles/d24'
+
+  get 'doubles/d25'
+
+  get 'doubles/d26'
+
+  get 'doubles/d27'
+
+  get 'doubles/d28'
+
+  get 'doubles/d29'
+
+  get 'doubles/d30'
+
+  get 'doubles/d31'
+
+  get 'doubles/d32'
+
+  get 'doubles/d33'
+
+  get 'doubles/d34'
+
+  get 'doubles/d35'
+
+  get 'doubles/d36'
+
+  get 'doubles/d37'
+
+  get 'doubles/d38'
+
+  get 'doubles/d39'
+
+  get 'doubles/d40'
+
+  get 'doubles/d41'
+
+  get 'doubles/d42'
+
+  get 'doubles/d43'
+
+  get 'doubles/d44'
+
+  get 'doubles/d45'
+
+  get 'doubles/d46'
+
+  get 'doubles/d47'
+
+  get 'doubles/d48'
+
+  get 'doubles/d49'
+
+  get 'doubles/d50'
+
+  get 'doubles/d51'
+
+  get 'doubles/d52'
+
+  get 'doubles/d53'
+
+  get 'doubles/d54'
+
+  get 'doubles/d55'
+
+  get 'doubles/d56'
+
+  get 'doubles/d57'
+
+  get 'doubles/d58'
+
+  get 'doubles/d59'
+
+  get 'doubles/d60'
+
+  get 'doubles/d61'
+
+  get 'doubles/d62'
+
+  get 'doubles/d63'
+
+  get 'doubles/d64'
+
+  get 'doubles/d65'
+
+  get 'doubles/d66'
+
+  get 'doubles/d67'
+
+  get 'doubles/d68'
+
+  get 'doubles/d69'
+
   get 'singles/s1'
 
   get 'singles/s2'
