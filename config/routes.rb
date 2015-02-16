78ec6025c5e5b87236a54d81750c7cd88005531e
polarpine <vanessabiggers@gmail.com>
2015-02-15 16:56:49 -0800

    add home route

--- config/routes.rb
+++ config/routes.rb
@@ -247,6 +247,8 @@ Rails.application.routes.draw do
 
   get'home/inspirations'
 
+  get 'home' => 'home#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
